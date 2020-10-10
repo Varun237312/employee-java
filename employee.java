fd2a9c7fa2a2375b7cf6815bca5d7f415137c546
Varun237312 <72202966+Varun237312@users.noreply.github.com>
2020-10-10 13:01:37 +0530

    Add files via upload

---
+++ employee.java
@@ -0,0 +1,31 @@
+package employee;
+
+public class employee {
+     String name;
+     String city;
+     int age;
+    
+    public static void main(String[] args) {
+      
+         employee e= new employee();
+         e.name="varun";
+         e.age=22;
+         e.city="dharwad";
+        
+        System.out.println("e.name");
+        System.out.println("e.age");
+        System.out.println("e.city");
+        
+        employee e1= new employee ();
+        e1.name="kiran";
+        e1.age=23;
+        e1.city="bengaluru";
+        
+        System.out.println("e1.name");
+        System.out.println("e1.age");
+        System.out.println("e1.city");
+        
+        
+   }
+    
+}
